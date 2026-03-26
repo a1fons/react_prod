3dd8bfcae8955b6606840cdff9883dcd7d052b25
Ture <turesafsten@gmail.com>
2026-03-26 14:14:21 +0100

    new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   src/pages/index.jsx 	new file:   vite.config.js

---
+++ src/pages/index.jsx
@@ -0,0 +1,54 @@
+import '../css/index_style.css'
+
+export default function Home(){
+        return  (
+        <main>
+            <div className="container-info">
+                <div className="container-text">
+                    <h3>Umeå naturvetar- och teknologkår</h3>
+                        <p>NTK är kåren för dig som studerar vid teknisk-naturvetenskaplig fakultet 
+                                vid Umeå Universitet. Vi finns till för att göra din studietid så bra  
+                                som möjligt. Detta gör vi bland annat genom att ordna roliga event, 
+                                göra studenternas röster hörda, driva kårhus och se till att studenterna 
+                                får bra näringslivsanknytning.</p> 
+                    <button onClick="showMessage()"> Bli medlem </button>
+                </div>
+                <img  src="images/Karhuset_Origo.jpg" alt="bild"/>
+            </div>
+
+            <div className="container-info">
+                <img  src="images/ovve_bild.png" alt="bild"/>
+                <div className="container-text">
+                    <h3>Studentlivet</h3>
+                    <p>NTK är kåren för dig som studerar vid teknisk-naturvetenskaplig fakultet 
+                        vid Umeå Universitet. Vi finns till för att göra din studietid så bra  
+                        som möjligt. Detta gör vi bland annat genom att ordna roliga event, 
+                        göra studenternas röster hörda, driva kårhus och se till att studenterna 
+                        får bra näringslivsanknytning. 
+                    </p>
+                
+                    <button onClick="window.location.href='studentliv.html'"> Info </button>
+                </div>
+            </div>
+
+            <div className="container-info-no-border">
+                <div className="container-text">
+                    <h3>Kårhuset Origo</h3>
+                    <p>Kårhuset Origo drivs av Umeå naturvetar- och teknologkår och är drygt 400 kvadratmeter stort. 
+                        Kårhuset kan på dagtid, 8-15, användas som  studieplats. 
+                        Givetvis har vi trådlös internetuppkoppling. Kårhuset är utrustat med 
+                        restaurangkök och bedriver  efter-plugget-pubverksamhet varje tisdag samt 
+                        nattklubb fredagar och  lördagar. Självklart så har vi ett stort och 
+                        varierat utbud av mat och  dryck. 
+                    </p>
+                    
+                    
+                    <button onClick="window.location.href='origo.html'" className="origo-button"> Info </button>
+                    
+                </div>
+                <img  src="images/Karhuset_Origo.jpg" alt="bild"/>
+            </div>
+        </main>
+    )
+}
+        
