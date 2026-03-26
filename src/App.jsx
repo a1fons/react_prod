d604ebd43b78c0df12e2571d84e7689b7e80b41e
Ture <turesafsten@gmail.com>
2026-03-26 14:37:16 +0100

    change stuff so it works installd react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/index';
 import './css/style.css'
 
 export default function App() {
